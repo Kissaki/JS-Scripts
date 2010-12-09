f59337ef81a68d1b149d6b8d41928be8ce57def4
Kissaki <kissaki@gmx.de>
2010-12-10 00:55:12 +0100

    update

--- urlencode.js
+++ urlencode.js
@@ -12,6 +12,7 @@ jQuery(document).ready(function(){
 		{from:/Ü/g, to: 'Ue'},
 		{from:/ß/g, to: 'ss'},
 		{from:/\n/g, to: ''},
+		{from:/[^a-zA-Z0-9]/g, to: '-'},
 	]
 	function onChange(eventObject){
 		source = jQuery(eventObject.target).attr('value');
@@ -19,22 +20,7 @@ jQuery(document).ready(function(){
 		
 		jQuery(replaceMap).each(function(index, element){
 			result = result.replace(element['from'], element['to']);
-			console.log('replace ' + element['from'] + ' to ' + element['to']);
-			
-			/*jQuery(element).each(function(i, e){
-				console.log('###replace ' + i + ' to ' + e);
-			});*/
 		});
-		for (var r in replaceMap) {
-			/*jQuery(r).each(function(index, element){
-				result = result.replace(index, element);
-				console.log('replace ' + index + ' to ' + element);
-			});*/
-			/*for (var k in replaceMap[r]) {
-				result = result.replace(r, replaceMap[r]);
-				console.log('replace ' + k + ' to ' + r[k]);
-			}*/
-		}
 		jQuery('#result').attr('value', result);
 	}
 	jQuery('#src').change(onChange).keyup(onChange);
